c39fca93871003887da1de279ada2a6ecfe857d9
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2026-05-13 16:59:37 +0200

    Extract main function for code clarity

--- bin/train_sin_nn.py
+++ bin/train_sin_nn.py
@@ -35,63 +35,82 @@ def _parse_args():
     return parser.parse_args()
 
 
-args = _parse_args()
-method_name = args.opt
-if method_name == "L-BFGS":
+def get_wrapper_loss_fn(loss_fn):
+    def wrapper_loss_fn(x, __):
+        return float(loss_fn(x))
+
+    return wrapper_loss_fn
+
+
+def get_wrapper_grad_fn(grad_fn):
+    def wrapper_grad_fn(x, grad_values, __):
+        grad_values[:] = np.array(grad_fn(x))
+
+    return wrapper_grad_fn
+
+
+def main():
+    args = _parse_args()
+    method_name = args.opt
+    if method_name == "L-BFGS":
+        if args.impl == "scipy_optimize":
+            method_name = "L-BFGS-B"
+        elif args.impl == "optim_jl":
+            method_name = "LBFGS"
+        else:
+            raise ValueError()
+
+    OUTDIR = start_experiment(args)
+
+    x, dx = np.linspace(-2 * np.pi, 2 * np.pi, num=101, retstep=True)
+    y = np.sin(x)
+    y_test = np.sin(x + 0.5 * dx)
+    x_2d = np.reshape(x, (len(x), -1))
+
+    mlp = MLP([1, 50, 1], seed=42, split_key=args.split_key)
+    misfit_loss = MisfitLoss(mlp, x_2d, y)
+    loss_fn = misfit_loss.loss
+    grad_fn = jax.grad(loss_fn)
+    wrapper_loss_fn = get_wrapper_loss_fn(loss_fn)
+    wrapper_grad_fn = get_wrapper_grad_fn(grad_fn)
+
+    s = Optim(args.impl)
+    s.set_initial_guess(np.asarray(mlp.theta, dtype=np.float64))
+    s.set_objective_fn(wrapper_loss_fn)
+    s.set_grad_fn(wrapper_grad_fn)
     if args.impl == "scipy_optimize":
-        method_name = "L-BFGS-B"
+        options = {"gtol": 1e-7}
     elif args.impl == "optim_jl":
-        method_name = "LBFGS"
+        options = {
+            "f_abstol": 2.2204460492503131e-09,
+            "g_abstol": 1e-7,
+            "iterations": 30_000,
+        }
     else:
         raise ValueError()
+    s.set_method(method_name, options)
 
-OUTDIR = start_experiment(args)
+    status, message = s.minimize()
 
-x, dx = np.linspace(-2 * np.pi, 2 * np.pi, num=101, retstep=True)
-y = np.sin(x)
-x_2d = np.reshape(x, (len(x), -1))
+    print("status = ", status)
+    print("message = ", message)
 
-mlp = MLP([1, 50, 1], seed=42, split_key=args.split_key)
-misfit_loss = MisfitLoss(mlp, x_2d, y)
-loss_fn = misfit_loss.loss
-grad_fn = jax.grad(loss_fn)
+    mlp.theta = s.x
+    pred = mlp.predict(x_2d)
+    pred = np.squeeze(pred)
+
+    print("Prediction error, 2-norm: ", np.linalg.norm(y_test - pred, 2) * np.sqrt(dx))
+
+    plt.plot(x, y, "-")
+    plt.plot(x, pred, "o")
+    plt.xlabel(r"$x$")
+    plt.ylabel(r"$\sin(x)$")
+    plt.tight_layout(pad=0.1)
+    plt.savefig(OUTDIR / "sin_func.png")
+    plt.show()
+
+    finish_experiment(OUTDIR)
 
 
-def wrapper_loss_fn(x, __):
-    return float(loss_fn(x))
-
-
-def wrapper_grad_fn(x, grad_values, __):
-    grad_values[:] = np.array(grad_fn(x))
-
-
-s = Optim(args.impl)
-s.set_initial_guess(np.asarray(mlp.theta, dtype=np.float64))
-s.set_objective_fn(wrapper_loss_fn)
-s.set_grad_fn(wrapper_grad_fn)
-if args.impl == "scipy_optimize":
-    options = {"gtol": 1e-7}
-elif args.impl == "optim_jl":
-    options = {"g_abstol": 1e-7}
-else:
-    raise ValueError()
-s.set_method(method_name, options)
-status, message = s.minimize()
-
-print("status = ", status)
-print("message = ", message)
-
-mlp.theta = s.x
-pred = mlp.predict(x_2d)
-pred = np.squeeze(pred)
-
-print("L2-error: ", np.linalg.norm(y - pred, 2) * np.sqrt(dx))
-
-plt.plot(x, y, "-")
-plt.plot(x, pred, "o")
-plt.xlabel(r"$x$")
-plt.ylabel(r"$\sin(x)$")
-plt.tight_layout(pad=0.1)
-plt.savefig(OUTDIR / "sin_func.pdf")
-
-finish_experiment(OUTDIR)
+if __name__ == "__main__":
+    main()
